4fec0709fcbc01853e59e70a3764bf2076fa606d
Alicja Potoniec <alicjahasinternet@outlook.com>
2026-05-27 00:47:55 +0100

    fixed syntax typos

--- loss_model.h
+++ loss_model.h
@@ -8,7 +8,7 @@ namespace sklearn_cpp{
 
     class LossModel{
         public:
-        virtual double compute_loss(const std::vector<std::vector<double>>& X, const std::vector<double>& Y, const std::vector<double>& w, double b) = 0;
+        virtual double computeLoss(const std::vector<std::vector<double>>& X, const std::vector<double>& Y, const std::vector<double>& w, double b) = 0;
 
         virtual ~LossModel()=default;
 
