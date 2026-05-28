85404e0326df28498da7d4580cacaa514f703a0a
LeandBlue <leandroukoh@yahoo.com>
2026-05-28 04:21:41 +0100

    I finished regression.cpp and regression.h. They will be revised after having a look at your files

--- Regression.h
+++ Regression.h
@@ -4,43 +4,66 @@
 #include <string>
 #include <cmath>
 
+class RegressionBase {
 
-class Regression {
-    private:
+    protected:
         std::vector<std::vector<double>> x;
         std::vector<double> y;
 
-        std::vector<double> w;
-        double b {0.0};
+        size_t m {};
 
         const double n {0.01};
         const int iteration {1000};
+    
+    public:
+        virtual void fit(const std::vector<std::vector<double>>& X,
+                        const std::vector<double>& Y) = 0;
+        
+        virtual ~RegressionBase() = default;
+};
 
-        size_t m;
+class Regression : public RegressionBase {
 
     protected:
-        virtual void compute_gradients(std::vector<double>& grad_w, 
-            double& grad_b) = 0;
-    public: 
-        Regression(std::vector<std::vector<double>> x, std::vector<double> y, 
-            std::vector<double> w) : x{x}, y{y}, w{w}, m{x.size()} {};
+        std::vector<double> w;
+        double b {0.0};
+
+        virtual void compute_gradients(std::vector<double>& derive_w, 
+            double& derive_b) = 0;
         
-        void gradientDescent() {
+        void solve() {
             for (size_t i = 0; i < iteration; i++) {
                 std::vector<double> derive_w(m, 0.0);
-                double derive_b = 0.0;
+                double derive_b {0.0};
 
                 compute_gradients(derive_w, derive_b);
 
                 for (size_t j = 0; j < m; j++) {
                     w[j] -= n * derive_w[j]; 
-                }
+                };
                 b  -= n * derive_b;
-            }
+            };
+        };
+    
+    public:         
+        void fit(const std::vector<std::vector<double>>& X,
+                const std::vector<double>& Y) override {
+                
+                x = X;
+                y = Y;
+                m = x[0].size();
+                w.assign(m,0.0);
+
+                solve();
+                };
+        
+        void GradientResult() {
+            std::cout << "===Gradient Descent Result===" << std::endl;
+            for (size_t j = 0; j < w.size(); j++) {
+                std::cout << "w" << j << " = " << w[j] << std::endl;
+            };
+            std::cout << "b = " << b << std::endl; 
         }
-    public:
-        virtual void fit(const std::vector<std::vector<double>>& X,
-             const std::vector<double>& Y) = 0;
 
         virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) = 0;
         
@@ -48,15 +71,83 @@ class Regression {
 };
 
 
-class Logistic : public Regression {
-    private:
-    double sigmoid{0.0};
-    public:
-        void compute_gradients(std::vector<double>& grad_w, 
-            double& grad_b) override;
 
-        void fit(const std::vector<std::vector<double>>& X,
-             const std::vector<double>& Y) override;
+class MulticlassRegression : public RegressionBase {
+
+    protected:
+        size_t K;       // number of classes
+
+        std::vector<std::vector<double>> w;
+        std::vector<double> b;
+
+        double lambda{0.01};
+
+        virtual void compute_gradient(std::vector<std::vector<double>>& derive_w, 
+            std::vector<double>& derive_b) = 0;
         
+        void solve() {
+            for (size_t i = 0; i < iteration; i++) {
+                std::vector<std::vector<double>> derive_w{K, std::vector<double>(m, 0.0)};
+                std::vector<double> derive_b(K, 0.0);
+
+                compute_gradient(derive_w, derive_b);
+
+                for (size_t j = 0; j < K; j++) {
+                    for (size_t k = 0; k < m; k++) {
+                        w[j][k] -= n * derive_w[j][k];
+                    }; 
+                    b[j]  -= n * derive_b[j];
+                };
+            };
+        };
+    public:
+        MulticlassRegression(size_t K = 1) : K{K} {};
+        void fit(const std::vector<std::vector<double>>& X,
+                const std::vector<double>& Y) override{
+                
+                x = X;
+                y = Y;
+                m = x[0].size();
+                w.assign(K, std::vector<double>(m,0.0));
+                b.assign(K,0.0);
+
+                solve();
+                };
+
+        virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) = 0;
+};
+
+class Linear : public Regression {
+    protected:
+        void compute_gradients(std::vector<double>& derive_w, 
+            double& derive_b) override;
+
+    public:               
+        virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) override;
+};
+
+class Logistic : public Regression {
+    protected:
+        void compute_gradients(std::vector<double>& derive_w, 
+            double& derive_b) override;
+    public:   
+        virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) override;
+};
+
+class MulticlassLogistic : public MulticlassRegression {
+    protected:
+        void compute_gradient(std::vector<std::vector<double>>& derive_w, 
+            std::vector<double>& derive_b) override;
+    public:
+        void GradientResult() {
+            std::cout << "===Multiclass Gradient Descent Result===" << std::endl;
+            for (size_t j = 0; j < K; j++) {
+                std::cout << "Class " << j << ": " << std::endl;
+                for (size_t k = 0; k < m; k++) {
+                    std::cout << "w" << j << "," << k << " = " << w[j][k] << std::endl;
+                }
+                std::cout << "b" << j << " = " << b[j] << std::endl;
+            }; 
+        };
         virtual std::vector<double> predict(const std::vector<std::vector<double>>& X) override;
 };
