1ae750c3edc84a8f160032622932ca39c1986cf6
Alicja Potoniec <alicjahasinternet@outlook.com>
2026-05-28 22:20:49 +0100

    Minor bug fixes

--- Multiclass_Logistic.h
+++ Multiclass_Logistic.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <cmath>
 
 #include "base_model.h"
 #include "loss_model.h"
@@ -11,30 +12,36 @@
 namespace sklearn_cpp {
     namespace linear_model {
         
-        class SoftmaxRegression : public BaseModel {
+        class LogRegSoftmax : public BaseModel {
             private:
                 LossModel& loss_model;
 
                 int K;
-                std::vector<std::vector<double>> w;
-                std::vector<double> b;
+                std::vector<std::vector<double>> W_multi;
+                std::vector<double> B_multi;
 
                 std::vector<double> softmax(const std::vector<double>& z) const {
                     std::vector<double> prob(z.size());
 
-                    double max_z = *std::max_element(z.begin(), z.end());
+                    // Finding the max value
+                    // can be replaced with a single line with double max_z = *std::max_element(z.begin(), z.end());
+                    // To do this the header would need to #include <algorithm>
+                    double max_z{z[0]};
+                    for(size_t i=1; i<z.size(); i++){
+                        if(z[i]>max_z) max_z = z[i];
+                    }
 
                     double sum {0.0};
                     for (double val : z) {
                         sum += std::exp(val - max_z);
-                    };
+                    }
 
                     for (size_t i = 0; i < z.size(); i++) {
                         prob[i] = std::exp(z[i] - max_z) / sum;
-                    };
+                    }
 
                 return prob;
-                };
+                }
             
             protected:
                 void compute_gradients(const std::vector<std::vector<double>>& X, const std::vector<double>& Y, std::vector<double>& grad_w, double& grad_b) override {}
@@ -44,56 +51,56 @@ namespace sklearn_cpp {
                         std::vector<double> z (K,0.0);
 
                         for (int k = 0; k < K; k++) {
-                            z[k] = b[k];
+                            z[k] = B_multi[k];
                             for (size_t j = 0; j < X[i].size(); j++) {
-                                z[k] += w[k][j] * X[i][j];                     
-                            };
-                        };
+                                z[k] += W_multi[k][j] * X[i][j];                     
+                            }
+                        }
 
                         std::vector<double> prob {softmax(z)};
 
                         for (int k = 0; k < K; k++) {
-                            double error = prob[k] - (Y[i] == k ? 1.0 :0.0);
+                            double error = prob[k] - (Y[i] == static_cast<double>(k) ? 1.0 :0.0);
                             for (size_t j = 0; j < X[i].size(); j++) {
                                 grad_w[k][j] += error * X[i][j];
-                            };
+                            }
 
                             grad_b[k] += error;
-                        };
-                    };
+                        }
+                    }
                 
                     double scale = 1.0 / X.size();
 
                     for (int k = 0; k < K; k++) {
-                        for (size_t j = 0; j < w[k].size(); j++) {
+                        for (size_t j = 0; j < W_multi[k].size(); j++) {
                             grad_w[k][j] *= scale;
-                        };
+                        }
 
                         grad_b[k] *= scale;
-                    };  
-                };
+                    }
+                }
                 
             public: 
-                SoftmaxRegression(int n_features,int K, LossModel& loss, double lr = 0.01, int iter = 1000): BaseModel{n_features, lr, iter}, loss_model{loss}, K{K} {
-                    w.resize(K, std::vector<double>(n_features, 0.0));
-                    b.resize(K,0.0);
+                LogRegSoftmax(int n_features,int K, LossModel& loss, double lr = 0.01, int iter = 1000): BaseModel{n_features, lr, iter}, loss_model{loss}, K{K} {
+                    W_multi.resize(K, std::vector<double>(n_features, 0.0));
+                    B_multi.resize(K,0.0);
                 }
                 
                 void fit(const std::vector<std::vector<double>>& X, const std::vector<double>& Y) {
                     for (int i = 0; i < iterations; i++) {
-                        std::vector<std::vector<double>> grad_w(K, std::vector<double>(w[0].size(), 0.0));
+                        std::vector<std::vector<double>> grad_w(K, std::vector<double>(W_multi[0].size(), 0.0));
                         std::vector<double> grad_b(K, 0.0);
 
                         compute_softmax_gradients(X, Y, grad_w, grad_b);
 
                         for (int k = 0; k < K; k++) {
-                            for (size_t j = 0; j < w[k].size(); j++) {
-                                w[k][j] -= learning_rate * grad_w[k][j];
-                            };
-                            b[k] -= learning_rate * grad_b[k];
-                        };
-                    };
-                };
+                            for (size_t j = 0; j < W_multi[k].size(); j++) {
+                                W_multi[k][j] -= learning_rate * grad_w[k][j];
+                            }
+                            B_multi[k] -= learning_rate * grad_b[k];
+                        }
+                    }
+                }
 
                 std::vector<double> predict(const std::vector<std::vector<double>>& X) const override {
 
@@ -105,11 +112,11 @@ namespace sklearn_cpp {
                         std::vector<double> z (K, 0.0);
 
                         for (int k = 0; k < K; k++) {
-                            z[k] = b[k];
+                            z[k] = B_multi[k];
                             for (size_t j = 0; j < X[i].size(); j++) {
-                                z[k] += w[k][j] * X[i][j];
-                            };
-                        };
+                                z[k] += W_multi[k][j] * X[i][j];
+                            }
+                        }
                         
                         std::vector<double> prob = softmax(z);
 
@@ -120,27 +127,28 @@ namespace sklearn_cpp {
                             if (prob[k] > bestProbability) {
                                 bestProbability = prob[k];
                                 bestClass = k;
-                            };
-                        };
+                            }
+                        }
                         
                         predictions.push_back(bestClass);
-                    };
+                    }
                     return predictions;
-                };
+                }
 
-                void printLoss() const {
+                void printLoss(const std::vector<std::vector<double>>& X, std::vector<double>& Y) const {
 
                     std::cout << std::fixed << std::setprecision(4);
 
                     std::cout << "=====Multiclass Logistic (Softmax) REGRESSION RESULTS=====\n";
                     for (int k = 0; k < K; k++) {
                         std::cout << "Class " << k << ":\n";
-                        std::cout << "b = " << b[k] << "\n";
-                    };
-                };
+                        std::cout << "b = " << B_multi[k] << "\n";
+                    }
+                    std::cout << "Loss: " << loss_model.computeLoss(X, Y, W_multi[0], B_multi[0]) << "\n";
+                }
         };
-    };
-};
+    }
+}
 
 
 #endif
